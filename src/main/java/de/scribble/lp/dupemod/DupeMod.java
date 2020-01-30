4f22877168e53712f6d6f4ddc26c1f81442e503e
Scribble <ScribbleLP@users.noreply.github.com>
2020-01-30 20:33:43 +0100

    Forgetting something again

--- src/main/java/de/scribble/lp/dupemod/DupeMod.java
+++ src/main/java/de/scribble/lp/dupemod/DupeMod.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class DupeMod
 {
     public static final String MODID = "dupemod";
-    public static final String VERSION = "1.6";
+    public static final String VERSION = "${version}";
     public static final String NAME= "DupeMod";
     public static Logger logger= LogManager.getLogger("DupeMod");
     private Minecraft mc = Minecraft.getMinecraft();
